6ecb7d5fa064e89d9f4ca30855e7bf83f044ed70
fudaylcavus <fudaylc@gmail.com>
2026-04-25 12:29:49 +0200

    test-remove

--- tests/setup.ts
+++
@@ -1,6 +0,0 @@
-import { vi } from 'vitest';
-import { MockRTCPeerConnection, MockMediaStream, MockMediaStreamTrack } from './mocks';
-
-vi.stubGlobal('RTCPeerConnection', MockRTCPeerConnection);
-vi.stubGlobal('MediaStream', MockMediaStream);
-vi.stubGlobal('MediaStreamTrack', MockMediaStreamTrack);
